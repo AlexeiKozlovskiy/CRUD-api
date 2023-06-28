0ad7afd07f87f86d0b782f34a4e5c6bfaee8c347
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-06-28 17:29:07 +0300

    feat: add env comonent

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+// import http from 'node:http';
 import http from 'http';
+import * as dotenv from 'dotenv';
 
-const PORT = 4000;
+dotenv.config();
+
+export const DEFAULT_PORT = 4000;
+
 const server = http.createServer(() => {});
 
+const PORT = process.env.PORT || DEFAULT_PORT;
 server.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
